d3047a7e5ded9e312d977e45a067738cd38f8aab
David León <114946370+daviiidleon@users.noreply.github.com>
2022-11-11 16:45:28 +0100

    Add files via upload

---
+++ 02_Strings/Ejercicio01.java
@@ -0,0 +1,24 @@
+/*
+
+ */
+package javaapplication45;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David <daviiidleongutierrez@gmail.com>
+ */
+public class JavaApplication45 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             
+           String texto = ("   Python rocks!");
+           System. out.println(texto.trim());
+                
+    }
+    
+}
---
+++ 02_Strings/Ejercicio02.java
@@ -0,0 +1,23 @@
+/*
+
+ */
+package ejercicio02sp;
+
+/**
+ *
+ * @author David <daviiidleongutierrez@gmail.com>
+ */
+public class Ejercicio02SP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        String str = "q q q q q q q q q q q q q q q q q q q q q q q q q w w w w w w w w w w w w w w w w w w w w w w w w w";
+        String resultado = str.replace(" ", "");
+        System.out.println(resultado);
+    }
+    
+}
