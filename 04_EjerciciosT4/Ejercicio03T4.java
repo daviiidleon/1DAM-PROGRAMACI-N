faab7658006bf53cf303e2d8f7da17ebec496cec
David León <114946370+daviiidleon@users.noreply.github.com>
2023-02-07 18:27:20 +0100

    Add files via upload

---
+++ 04_EjerciciosT4/Ejercicio03T4.java
@@ -0,0 +1,20 @@
+/*
+
+ */
+
+package dl.t2.ejercicio03t4a;
+
+import daw.persona.MarcadorBaloncesto;
+
+/**
+ *
+ * @author David <daviiidleongutierrez@gmail.com>
+ */
+public class Ejercicio03T4a {
+
+    public static void main(String[] args) {
+        
+        MarcadorBaloncesto mb = new MarcadorBaloncesto("Valencia", "Granada");
+            System.out.println(mb.getNombreLocal());
+    }
+}
